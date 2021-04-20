682d62b325e71d210fb76aa4bf2877760b074c98
Adam Sztandar <10097355+asztandar@users.noreply.github.com>
2021-04-20 23:28:05 +0200

    Added sorting

--- lab3/dataListAndProgressSupport/components/sortowanie.js
+++ lab3/dataListAndProgressSupport/components/sortowanie.js
@@ -1,9 +1,38 @@
 import React from 'react';  
-import {Text, View} from 'react-native';
+import {Text, View, ScrollView, Button} from 'react-native';
 
 import styles from "../style/stylesheet"
- const Sortowanie = props => {
-   
+ 
+let liczbyRandom = [];
+let sorted = [];
+let klikniety = false;
+
+function onPressHandle(){
+    klikniety = true;
+}
+
+function GenerateNumbers(){
+    for(let i=0;i<100;i++){
+        liczbyRandom.push(Math.floor(Math.random()* 1000))
+        console.log(liczbyRandom[i])
+        sorted = liczbyRandom;
+    }
+    
+}
+function compareNumbers(a, b) {
+    return a - b
+ }
+
+function sortuj(){
+    sorted.sort(compareNumbers)
+    console.log({sorted})
+}
+
+
+
+const Sortowanie = props => {
+   GenerateNumbers();
+   sortuj();
  return (  
         <View style={styles.containerSortowanie}>  
        
@@ -11,7 +40,17 @@ import styles from "../style/stylesheet"
            <Text>
                <Text style={styles.bold}>Sortowanie i filtrowanie danych</Text>
            </Text>
+           
        </View>
+       <ScrollView>
+           {liczbyRandom.map(liczba => {return (
+               <Text>{liczba},</Text>
+           )})}
+       <Text>Posortowane:</Text>
+       {sorted.map(liczba => {return (
+               <Text>{liczba},</Text>
+           )})}
+       </ScrollView>
          
         </View>  
     );  
